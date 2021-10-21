b9c1c91bb71b57f2c9c3d7e907c8590b538ed24f
Anthony69600 <a.verchere69@gmail.com>
2021-10-21 09:48:40 +0200

    first commit

--- index.php
+++ index.php
@@ -2,20 +2,25 @@
 // index.php bicycle
 
 require 'Bicycle.php';
-require 'Car.php';
-require 'Truck.php';
+
+// index.php Skateboard
+
+require_once 'Skateboard.php';
+
+$skate = new Skateboard ('white', 1);
+
 
 $bicycle = new Bicycle('blue', 1);
-echo $bicycle->forward();
-
 
 // index.php Car
 
+require 'Car.php';
 
 $car = new Car('green', 4, 'electric');
-echo $car->forward();
 
 // index.php Truck
+
+require 'Truck.php';
 
 $truck = new Truck('red', 3, 35,'fuel');
 
@@ -23,11 +28,52 @@ $truck->setCurrentSpeed(50);
 $truck->setNbWheels(8);
 
 
-var_dump($truck);
+// Les classes highway
 
-        echo $truck->forward();
-        echo $truck->brake();
-        echo $truck->getLoad();
+require "MotorWay.php";
+require "PedestrianWay.php";
+require "ResidentialWay.php";
+
+
+$highway = new MotorWay();
+$pedestrianway = new PedestrianWay();
+$cityway = new ResidentialWay();
+
+echo '<br><br>';
+
+$cityway->addVehicle($car);
+
+echo '<br><br>';
+
+$cityway->addVehicle($truck);
+
+echo '<br><br>';
+
+var_dump($cityway->getCurrentVehicles());
+
+echo '<br><br>';
+
+$cityway->addvehicle($bicycle);
+
+echo '<br><br>';
+
+$highway->addvehicle($truck);
+
+var_dump($pedestrianway->getCurrentVehicles());
+
+echo '<br><br>';
+
+$pedestrianway->addvehicle($skate);
+
+echo '<br><br>';
+
+var_dump($pedestrianway->getCurrentVehicles());
+
+
+
+
+
+
 
 
 ?>
